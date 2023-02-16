83590d5475789e22ca9b37ce657da8c28d249378
Ghanshyam Vaja <ghanshyamvaja11@gmail.com>
2023-02-16 07:49:39 +0530

    third commit

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://g-chat-ajan.onrender.com', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
